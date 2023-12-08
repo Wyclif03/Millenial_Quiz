7b9df82ed124d591c12e92ae6f571a381c546388
Wyclif03 <ppierson06@hotmail.com>
2023-12-08 15:30:35 +0000

    new commit

---
+++ data.js
@@ -0,0 +1,50 @@
+const questions = [
+  {
+    text: "You need to get across town quickly. You:",
+    answers: [
+      { text: "Summon an Uber while sharing your frustration on TikTok about public transit delays" },
+      { text: "Hop on an electric scooter, recording the experience for your next Snapchat story" },
+      { text: "Saunter casually while documenting your journey on Instagram Stories, complete with filters and emojis" }
+    ]
+  },
+  {
+    text: "What's your ultimate comfort food?",
+    answers: [
+      { text: "Vegan avocado toast with a side of kale chips – because self-care starts with a healthy meal" },
+      { text: "Classic mac 'n' cheese – because some things never go out of style" },
+      { text: "Quinoa salad with a touch of organic balsamic vinaigrette – because you're a culinary adventurer" }
+    ]
+  },
+  {
+    text: "You just got paid. Your first move is to:",
+    answers: [
+      { text: "Venmo your friends your share of the bill from the avocado toast brunch you all shared" },
+      { text: "Contemplate quitting your day job to become a full-time content creator on YouTube" },
+      { text: "Open a high-yield savings account and secretly judge those who spend money on experiences instead of saving for the future" }
+    ]
+  },
+  {
+    text: "Your friend invites you to hang out in the group chat. You respond by:",
+    answers: [
+      { text: "Proposing a group TikTok challenge and brainstorming potential viral dance moves" },
+      { text: "Claiming you're too busy binge-watching the latest streaming sensation to join the real world" },
+      { text: "Replying with a perfectly curated Spotify playlist, complete with album artwork and track descriptions" }
+    ]
+  },
+  {
+    text: "What would be your ideal weekend activity?",
+    answers: [
+      { text: "Attending a music festival with friends – because the louder, the better" },
+      { text: "Exploring a cozy bookstore and getting lost in a good novel – because books are timeless" },
+      { text: "Organizing a DIY home improvement project – because nothing beats a well-arranged living space" }
+    ]
+  }
+];
+
+const results = [
+  "Mostly A’s: Congratulations! You're so Gen Z that your spirit animal is a meme, and your communication style is fluent in emojis and hashtags.",
+  "Mostly B’s: You're a certified millennial! You remember the days of dial-up internet, flip phones, and the struggle of deciding your Top 8 on MySpace.",
+  "Mostly C’s: You're rocking the Gen X vibes with your color-coded calendar, sensible financial decisions, and skepticism about TikTok dances."
+];
+
+export { questions, results };
